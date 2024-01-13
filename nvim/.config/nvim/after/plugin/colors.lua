d49062bfed9f9e8165147b7cc0e9486115bfd339
Jon Mosco <jmosco@redhat.com>
2024-01-13 10:29:41 -0500

    tmux changes for stowing

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,11 +1,4 @@
--- require('neo_sola'navarasu/onedark.nvim'rized').set()
-
 require('colorbuddy').setup()
-
--- require('neosolarized').setup({
---     comment_italics = true,
---     background_set = false,
--- })
 
 -- vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#b58900'} )
 
@@ -17,9 +10,48 @@ require('colorbuddy').setup()
 -- require('nord').set()
 
 
-require('onedark').setup  {
-    -- Main options --
-    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true,
-}
-require('onedark').load()
+-- require('onedark').setup  {
+--     -- Main options --
+--     style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+--     transparent = true,
+-- }
+-- require('onedark').load()
+
+-- Default options
+require('nightfox').setup({
+  options = {
+      fox = nord,
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = true,     -- Disable setting background
+    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = true,    -- Non focused panes set to alternative background
+    module_default = true,   -- Default enable value for modules
+    styles = {               -- Style to be applied to different syntax groups
+      comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+})
+
+-- setup must be called before loading
+vim.cmd("colorscheme nordfox")
