e2c41a841e2d5aef3cbbe222d5b97bbdc7f527a8
Jon Mosco <jmosco@redhat.com>
2026-08-01 19:43:48 -0400

    add keybinding and remove unused file

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,13 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>pws', function()
+    builtin.grep_string({ search = vim.fn.expand("<cword>") })
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
 require("telescope").load_extension("ui-select")
